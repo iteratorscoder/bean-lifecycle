e42cc53758f152c5f95a3e7e51435e43d8b234ea
iterators <iterators@ushareit.com>
2020-05-03 12:03:01 +0800

    :tada: bean lifecycle: init method with @PostConstruct

--- src/main/java/com/iterators/bean/lifecycle/init/MyInitializingBean.java
+++ src/main/java/com/iterators/bean/lifecycle/init/MyInitializingBean.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @author iterators
  * @time 2020.05.03
@@ -12,16 +14,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyInitializingBean implements InitializingBean {
 
+    public static final String TAG = MyInitializingBean.class.getSimpleName();
+
     static {
-        log.info("{} exec static block code", MyInitializingBean.class.getName());
+        log.info("{} exec static block code", TAG);
     }
 
     public MyInitializingBean() {
-        log.info("{} exec ctor code", MyInitializingBean.class.getName());
+        log.info("{} exec ctor code", TAG);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        log.info("{} exec afterPropertiesSet code", MyInitializingBean.class.getName());
+        log.info("{} exec afterPropertiesSet code", TAG);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("{} exec init code", TAG);
     }
 }
