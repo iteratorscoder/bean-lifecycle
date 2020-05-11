39dc877e213c86ac068e170775dd84c30e9898c4
iterators <iterators@ushareit.com>
2020-05-11 13:17:32 +0800

    :tada: refine api

--- src/main/java/com/iterators/bean/lifecycle/selector/HelloWorldSelector.java
+++ src/main/java/com/iterators/bean/lifecycle/selector/HelloWorldSelector.java
@@ -3,11 +3,10 @@ package com.iterators.bean.lifecycle.selector;
 import com.iterators.bean.lifecycle.service.HelloWorldService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.annotation.MergedAnnotation;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.lang.NonNull;
-
-import java.util.Objects;
 
 /**
  * selectImports参数importingClassMetadata指的是哪个类通过注解引入了HelloWorldSelector，此处是AppConfig
@@ -24,10 +23,12 @@ public class HelloWorldSelector implements ImportSelector {
         // 打印出importingClassMetadata具有的所有注解的信息
         importingClassMetadata.getAnnotations().forEach((HelloWorldSelector::logInfo));
         // 获取注解属性值
-        Objects.requireNonNull(importingClassMetadata.getAnnotationAttributes(EnableAutoHelloWorldConfiguration.class.getName()))
-                .forEach((key, value) -> {
-                    log.info("key: {}, value: {}", key, value);
-                });
+        AnnotationAttributes attributes = AnnotationAttributes.fromMap(importingClassMetadata.getAnnotationAttributes(EnableAutoHelloWorldConfiguration.class.getName(), false));
+        if (attributes != null) {
+            attributes.forEach((key, value) -> {
+                log.info("key: {}, value: {}", key, value);
+            });
+        }
         String autoHello = HelloWorldService.class.getName();
         return new String[]{autoHello};
     }
